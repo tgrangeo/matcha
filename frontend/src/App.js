129f20dc50dc7ddf6bfd53c26379e068ebb2a4e4
edep <eliott.dpw@gmail.com>
2023-03-13 14:13:22 +0100

    setup front

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="header">
+        header
+        {
+          //logo accueil
+        }
+        {
+          //CTA se connecter ou profil image/nom (components Profil)
+        }
+      </header>
+      <section className="content">
+
+      </section>
+      <footer>
+        footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import style from './style.module.scss';
+
+const Card = ({ name }) => {
+  return (
+    <div className={style.card}>
+      EE
+    </div>
+  );
+}
+
+export default Card;
